99ae0b2e50b835a97add18a7b28aa1c7fc0a843e
cjsampe <samperiz@hotmail.com>
2022-11-19 08:36:52 +0100

    routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+//nuevo
+
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+//import { BootstrapVue } from 'bootstrap-vue'
+
+
+
+
+//nuevo
 import router from './router'
 import store from './store'
+
+
 
 Vue.config.productionTip = false
 
@@ -10,3 +25,8 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+//nuevo
+// Make BootstrapVue available throughout your project
+//Vue.use(BootstrapVue)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="home">
+    <div class="row">
+      <div class="col-8">
+        <RouteList></RouteList>
+      </div></div>
+      <AppTipVue></AppTipVue>
+    
+    
+  </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import RouteList from "../components/RouteList.vue";
+import AppTipVue from '@/components/AppTip.vue';
+
+export default {
+  name: "HomeView",
+  components: {
+    RouteList,
+    AppTipVue
+  },
+}
+  
+</script>
+<!-- ANTERIORRRRRR
+<template>
+  <div class="home">
   <AppTipVue></AppTipVue>
     
   </div>
@@ -15,3 +44,5 @@ export default {
   }
 }
 </script>
+
+-->
